136f0074e8592208097cadac3ff9f7b288660e28
shina <rasheedshinaopeyemi@gmail.com>
2021-07-08 16:57:26 +0100

    add the registration model, controller and routes and completed the registration process

--- config/db.js
+++ config/db.js
@@ -3,19 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionString =
-  process.env.NODE_ENV === 'development'
-    ? process.env.MONGO_URI_DEV
-    : process.env.MONGO_URI_PROD;
+// const connectionString =
+//   process.env.NODE_ENV === 'development'
+//     ? process.env.MONGO_URI_DEV
+//     : process.env.MONGO_URI_PROD;
 
 async function connectDb() {
   try {
-    const conn = await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    const conn = await mongoose.connect(
+      'mongodb+srv://rasheedopeyemi:opeyemi22@@cluster0.9m6fq.mongodb.net/skyviewcapital?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }
+    );
     console.log(`Mongodb connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(`Error: ${error.message}`.red.underline.bold);
